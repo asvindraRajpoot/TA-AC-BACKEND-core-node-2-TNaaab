652ad6ab12c16108c401526309c9d93cd4c62779
asvindra <aswin1399904@gmail.com>
2021-10-21 21:50:00 +0530

    Completed the absolute and relative path

--- block-BNaaax/server.js
+++ block-BNaaax/server.js
@@ -3,7 +3,7 @@
 // 2. console.log(\_\_filename);
 // 3. use path module to join `__dirname` and `server.js`
 console.log(__dirname);
-//console.log(server.js);
+console.log(__filename);
 var path=require('path');
-let modulePath=path.join(__dirname,server.js);
+let modulePath=path.join(__dirname,'server.js');
 console.log(modulePath);
